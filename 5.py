4c3bfc81037b9699d825d590c991f2818cd0577a
fandee <pavlo.protsyshyn@gmail.com>
2022-07-01 14:09:52 +0300

    5

---
+++ 5.py
@@ -0,0 +1,27 @@
+'''
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+'''
+
+'''
+20 = 2^2 * 5
+19 = 19
+18 = 2 * 3^2
+17 = 17
+16 = 2^4
+15 = 3 * 5
+14 = 2 * 7
+13 = 13
+12 = 2^2 * 3
+11 = 11
+9 = 3^2
+8 = 2^3
+7
+6 = 2 * 3
+4 = 2^2
+3
+2
+'''
+# mathematics rule
+print(2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19)
